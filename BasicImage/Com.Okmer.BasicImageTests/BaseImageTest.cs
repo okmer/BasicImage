f1e68bd3344c84af4e9ab95f24471e82c31a4842
Remko van Brakel <rvbr@allseas.com>
2022-12-01 15:54:05 +0100

    Added Unit Tests for the GetPixelSpan and GetLineSpan.

--- BasicImage/Com.Okmer.BasicImageTests/BaseImageTest.cs
+++ BasicImage/Com.Okmer.BasicImageTests/BaseImageTest.cs
@@ -4,7 +4,7 @@ namespace Com.Okmer.BasicImageTests
     public class BaseImageTest
     {
         [TestMethod]
-        public void CreateImage()
+        public void CreateBaseImage()
         {
             int width = 640;
             int height = 480;
@@ -21,6 +21,134 @@ namespace Com.Okmer.BasicImageTests
 
             Assert.IsNotNull(image.Data);
             Assert.IsTrue(image.Data.Length >= width * height * channels);
+        }
+
+        [TestMethod]
+        public void GetPixelSpan_Channels1()
+        {
+            int width = 3;
+            int height = 3;
+            int channels = 1;
+
+            var data = new byte[] {
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9, };
+
+            using var image = new BaseImage<byte>(width, height, channels, data);
+
+            var pixel1 = image.GetPixelSpan(0, 0);
+            var pixel5 = image.GetPixelSpan(1, 1);
+            var pixel9 = image.GetPixelSpan(2, 2);
+
+            Assert.IsTrue(image.IsValid);
+            Assert.IsNotNull(image.Data);
+
+            Assert.AreEqual(pixel1.Length, channels);
+            Assert.AreEqual(pixel5.Length, channels);
+            Assert.AreEqual(pixel9.Length, channels);
+
+            Assert.AreEqual(pixel1[0], data[0]);
+            Assert.AreEqual(pixel5[0], data[4]);
+            Assert.AreEqual(pixel9[0], data[8]);
+        }
+
+        [TestMethod]
+        public void GetPixelSpan_Channels3()
+        {
+            int width = 3;
+            int height = 1;
+            int channels = 3;
+
+            var data = new byte[] {
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9, };
+
+            using var image = new BaseImage<byte>(width, height, channels, data);
+
+            var pixel1 = image.GetPixelSpan(0, 0);
+            var pixel2 = image.GetPixelSpan(1, 0);
+            var pixel3 = image.GetPixelSpan(2, 0);
+
+            Assert.IsTrue(image.IsValid);
+            Assert.IsNotNull(image.Data);
+
+            Assert.AreEqual(pixel1.Length, channels);
+            Assert.AreEqual(pixel2.Length, channels);
+            Assert.AreEqual(pixel3.Length, channels);
+
+            Assert.AreEqual(pixel1[0], data[0]);
+            Assert.AreEqual(pixel1[1], data[1]);
+            Assert.AreEqual(pixel1[2], data[2]);
+
+            Assert.AreEqual(pixel2[0], data[3]);
+            Assert.AreEqual(pixel2[1], data[4]);
+            Assert.AreEqual(pixel2[2], data[5]);
+
+            Assert.AreEqual(pixel3[0], data[6]);
+            Assert.AreEqual(pixel3[1], data[7]);
+            Assert.AreEqual(pixel3[2], data[8]);
+        }
+
+        [TestMethod]
+        public void GetLineSpan_Channels1()
+        {
+            int width = 3;
+            int height = 3;
+            int channels = 1;
+
+            var data = new byte[] {
+                1, 2, 3,
+                4, 5, 6,
+                7, 8, 9, };
+
+            using var image = new BaseImage<byte>(width, height, channels, data);
+
+            var line1 = image.GetLineSpan(0);
+            var line2 = image.GetLineSpan(1);
+            var line3 = image.GetLineSpan(2);
+
+            Assert.IsTrue(image.IsValid);
+            Assert.IsNotNull(image.Data);
+
+            Assert.AreEqual(line1.Length, width * channels);
+            Assert.AreEqual(line2.Length, width * channels);
+            Assert.AreEqual(line3.Length, width * channels);
+
+            CollectionAssert.AreEqual(line1.ToArray(), data.Take(width * channels).ToArray());
+            CollectionAssert.AreEqual(line2.ToArray(), data.Skip(width * channels).Take(width * channels).ToArray());
+            CollectionAssert.AreEqual(line3.ToArray(), data.Skip(2 * width * channels).Take(width * channels).ToArray());
+        }
+
+        [TestMethod]
+        public void GetLineSpan_Channels3()
+        {
+            int width = 3;
+            int height = 3;
+            int channels = 3;
+
+            var data = new byte[] {
+                1, 2, 3,  4, 5, 6,  7, 8, 9,
+                11, 12, 13,  14, 15, 16,  17, 18, 19,
+                21, 22, 23,  24, 25, 26,  27, 28, 29, };
+
+            using var image = new BaseImage<byte>(width, height, channels, data);
+
+            var line1 = image.GetLineSpan(0);
+            var line2 = image.GetLineSpan(1);
+            var line3 = image.GetLineSpan(2);
+
+            Assert.IsTrue(image.IsValid);
+            Assert.IsNotNull(image.Data);
+
+            Assert.AreEqual(line1.Length, width * channels);
+            Assert.AreEqual(line2.Length, width * channels);
+            Assert.AreEqual(line3.Length, width * channels);
+
+            CollectionAssert.AreEqual(line1.ToArray(), data.Take(width * channels).ToArray());
+            CollectionAssert.AreEqual(line2.ToArray(), data.Skip(width * channels).Take(width * channels).ToArray());
+            CollectionAssert.AreEqual(line3.ToArray(), data.Skip(2 * width * channels).Take(width * channels).ToArray());
         }
     }
 }
