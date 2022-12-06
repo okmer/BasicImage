98b90cc59ff0ea7fb2fc57344c4bc2df5d35f28b
Remko van Brakel <rvbr@allseas.com>
2022-12-06 18:37:24 +0100

    Added ClearPadding to clean the borders (padded area) of convolution (processed) images.

---
+++ BasicImage/Com.Okmer.BasicImage.Convolution/Extensions/PaddedImageExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Okmer.BasicImage.Convolution;
+
+public static class PaddedImageExtensions
+{
+    public static void ClearPadding<T>(this PaddedImage<T> image)
+    {
+        if (!image.IsValid) throw new ArgumentException($"{nameof(ClearPadding)}: {nameof(image)} is NOT valid.");
+
+        if (image.Padding == 0) return;
+
+        int topBottomRange = image.Padding * image.Stride;
+
+        int topStart = 0;
+        int bottomStart = image.Height * image.Stride - topBottomRange;
+
+        //Clear top line(s)
+        var topSpan = new Span<T>(image.Data, topStart, topBottomRange);
+        topSpan.Clear();
+
+        //Clear bottom line(s)
+        var bottomSpan = new Span<T>(image.Data, bottomStart, topBottomRange);
+        bottomSpan.Clear();
+
+        int stride = image.Stride;
+        int leftRightRange = image.Padding * image.Channels;
+
+        //Clear left and right column(s)
+        for (int y = image.Padding; y < image.Height; y++)
+        {
+            int lineIndex = y * stride;
+                
+            var leftSpan = new Span<T>(image.Data, lineIndex, leftRightRange);
+            leftSpan.Clear();
+
+            var rightSpan = new Span<T>(image.Data, lineIndex - leftRightRange, leftRightRange);
+            rightSpan.Clear();
+        }
+    }
+}
+
---
+++ BasicImage/Com.Okmer.BasicImageTests/PaddedImageExtensionsTest.cs
@@ -0,0 +1,81 @@
+﻿using Com.Okmer.BasicImage.Convolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Okmer.BasicImageTests;
+
+[TestClass]
+public class PaddedImageExtensionsTest
+{
+    [TestMethod]
+    public void ClearPadding_Channels1a()
+    {
+        var input = new byte[] {
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9 };
+
+        var output = new byte[] {
+        0, 0, 0,
+        0, 5, 0,
+        0, 0, 0 };
+
+        using var image = new BaseImage<byte>(3, 3, 1, input);
+
+        using var padded = new PaddedImage<byte>(image, 1);
+
+        padded.ClearPadding();
+
+        CollectionAssert.AreEqual(image.Data?.Take(input.Length).ToArray(), input);
+        CollectionAssert.AreEqual(padded.Data?.Take(output.Length).ToArray(), output);
+    }
+
+    [TestMethod]
+    public void ClearPadding_Channels1b()
+    {
+        var input = new byte[] {
+            1, 2, 3,  4, 5, 6,  7, 8, 9,
+            11, 12, 13,  14, 15, 16,  17, 18, 19,
+            21, 22, 23,  24, 25, 26,  27, 28, 29, };
+
+        var output = new byte[] {
+            0, 0, 0,  0, 0, 0,   0, 0, 0,
+            0, 12, 13,  14, 15, 16,  17, 18, 0,
+            0, 0, 0,  0, 0, 0,   0, 0, 0, };
+
+        using var image = new BaseImage<byte>(9, 3, 1, input);
+
+        using var padded = new PaddedImage<byte>(image, 1);
+
+        padded.ClearPadding();
+
+        CollectionAssert.AreEqual(image.Data?.Take(input.Length).ToArray(), input);
+        CollectionAssert.AreEqual(padded.Data?.Take(output.Length).ToArray(), output);
+    }
+
+    [TestMethod]
+    public void ClearPadding_Channels3()
+    {
+        var input = new byte[] {
+            1, 2, 3,  4, 5, 6,  7, 8, 9,
+            11, 12, 13,  14, 15, 16,  17, 18, 19,
+            21, 22, 23,  24, 25, 26,  27, 28, 29, };
+
+        var output = new byte[] {
+            0, 0, 0,  0, 0, 0,   0, 0, 0,
+            0, 0, 0,  14, 15, 16,  0, 0, 0,
+            0, 0, 0,  0, 0, 0,   0, 0, 0, };
+
+        using var image = new BaseImage<byte>(3, 3, 3, input);
+
+        using var padded = new PaddedImage<byte>(image, 1);
+
+        padded.ClearPadding();
+
+        CollectionAssert.AreEqual(image.Data?.Take(input.Length).ToArray(), input);
+        CollectionAssert.AreEqual(padded.Data?.Take(output.Length).ToArray(), output);
+    }
+}
